d5c6a195a26c1a7551ba399d1ee6e8ddc6561e28
Liu Chang <SmithLiu95@users.noreply.github.com>
2018-11-02 11:02:26 +0800

    Add files via upload

--- generate_data.py
+++ generate_data.py
@@ -28,25 +28,18 @@ import os.path as path
 import ndjson
 
 dataset_path='/raid5/liuchang/quick_draw'
-class_name = ['calender', 'snowman', 'penguin', 'blackberry', 'teddy-bear']  # class_name=-1 means all class
-country_code = ['JP', 'CN', 'DE']  # country_code=-1 means all countries
+class_name = ['calendar', 'snowman', 'penguin', 'blackberry', 'teddy-bear']  # class_name=-1 means all class
+country_code = -1  # country_code=-1 means all countries
 output_path='/raid5/liuchang/quick_draw_output'
 
 
-def split_data(country_code,data):
-    splitted=[]
-    if isinstance(country_code,str):
-        country_code=country_code.split(' ')
-    for i in data:
-        if country_code!=-1 and i['countrycode']  not in country_code:
-            continue
-        # if i['recognized']==False:
-        #     continue
-        splitted.append((i['drawing'],i['word']))
-    return splitted
+import numpy as np
+def split_data(data):
+    return [(i['recognized'],i['drawing'],i['word']) for i in data]
 
 splitted=[]
 i =0
+min_len=np.inf
 for file_path in glob.iglob(path.join(dataset_path,'*.ndjson')):
     i += 1
     label=file_path.split('/')[-1].split('.')[0]
@@ -55,22 +48,39 @@ for file_path in glob.iglob(path.join(dataset_path,'*.ndjson')):
     print('readding data from {}'.format(file_path))
     with open(file_path) as f:
         data = ndjson.load(f)
-    # for i in data[:4]:
-    #     print(i)
-    splitted+=split_data(country_code,data)
-    print('Now found {} avaliable data.'.format(len(splitted)))
-
+    print('Now found {} avaliable data.'.format(len(data)))
+    splitted.append(split_data(data))
+    min_len=min(min_len,len(data))
 
 
 import random
-random.shuffle(splitted)
+dataset=[]
+for i,data in enumerate(splitted):
+    dataset+=random.sample(data,min_len)
+random.shuffle(dataset)
+print(min_len,len(dataset))
+Recognized=[i[0] for i in dataset]
+X=[i[1] for i in dataset]
+Y=[i[2] for i in dataset]
 
-X=[i[0] for i in splitted]
-Y=[i[1] for i in splitted]
+Yu = set(Y)
+dict = {}
+for index, label in enumerate(Yu):
+    dict[label] = index
+
+Yc = []
+for i in Y:
+    Yc.append(dict[i])
+
+r=int(0.8*len(X))
+train_Recognized=Recognized[:r]
+train_X=X[:r]
+train_Y=Yc[:r]
+test_Recognized=Recognized[r:]
+test_X=X[r:]
+test_Y=Yc[r:]
 
 import pickle
-with open(path.join(output_path,'data_X'),'wb') as f:
-    pickle.dump(X,f)
-
-with open(path.join(output_path,'data_Y'),'wb') as f:
-    pickle.dump(Y,f)
+import os
+with open(os.path.join(output_path,'1102_05b'+str(len(X))),'wb') as f:
+    pickle.dump((train_Recognized,train_X,train_Y,test_Recognized,test_X,test_Y),f)
