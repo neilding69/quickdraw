555552674853b1f657583ba11d7d1a7759ee817d
Liu Chang <SmithLiu95@users.noreply.github.com>
2018-11-03 13:12:05 +0800

    Add files via upload

--- utils/read_data.py
+++ utils/read_data.py
@@ -16,8 +16,13 @@ In get_dataset(), train_r=(0,1] indicates how much you want to use in training d
 """
 import os
 import pickle
+
+def _cut_stroke(x,ratio=1):
+    if ratio==1:
+        return x
+    return [i[:int(len(i)*ratio)] for i in x]
 def get_dataset(dataset_path='/raid5/liuchang/quick_draw_output/',dataset_name='1102_05b640765',train_r=1.0,test_r=1.0):
     (train_Recognized, train_X, train_Y, test_Recognized, test_X, test_Y)=pickle.load(open(os.path.join(dataset_path,dataset_name),'rb'))
-    train_r=int(train_r*len(train_X))
-    test_r=int(test_r*len(test_X))
+    train_X=_cut_stroke(train_X,train_r)
+    test_X=_cut_stroke(test_X,test_r)
     return train_X[:train_r],train_Y[:train_r],test_X[:test_r],test_Y[:test_r]
